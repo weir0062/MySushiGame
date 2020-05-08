f73d8b29377b67d651c68f3b2bc60cfd66ea2ce2
Charlie <weir0062@algonquinlive.com>
2020-05-08 17:10:45 -0400

    Fixed problem and did a bit of work

---
+++ Assets/Scipts/SushiScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class SushiScript : MonoBehaviour
+{
+    public int TunaRollValue = 100;
+    public int RiceBallValue = 50;
+    public int BombValue = -500;
+    public Material TunaRollMaterial;
+    public Material RiceBallMaterial;
+    public Material BombMaterial;
+    public float MinSpeed=2;
+    public float MaxSpeed=10;
+    public float MinRotationSpeed = 10;
+    public float MaxRotationSpeed = 40;
+    private int m_value;
+    private float m_verticalSpeed;
+    private float m_RotationSpeed;
+    public enum e_SushiType
+    {
+        tuna_roll = 0,
+        rice_ball,
+        bomb,
+    }
+   
+    public void Initialize(e_SushiType e_Sushi)
+    {
+        m_verticalSpeed = Random.Range(MinSpeed, MaxSpeed);
+        m_RotationSpeed = Random.Range(MinRotationSpeed, MaxRotationSpeed);
+
+        switch (e_Sushi)
+        {
+            case e_SushiType.tuna_roll:
+                {
+                    m_value = TunaRollValue;
+                    GetComponent<MeshRenderer>().material = TunaRollMaterial;
+                    break;
+                }
+            case e_SushiType.rice_ball:
+                {
+                    m_value = RiceBallValue;
+                    GetComponent<MeshRenderer>().material = RiceBallMaterial;
+                    break;
+                }
+            case e_SushiType.bomb:
+                {
+                    m_value = BombValue;
+                    GetComponent<MeshRenderer>().material = BombMaterial;
+                    break;
+                }
+            default:
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize(e_SushiType.tuna_roll);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * m_verticalSpeed * Time.deltaTime, Space.World);
+
+        transform.Rotate(Vector3.back * m_RotationSpeed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scipts/SushiSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SushiSpawner : MonoBehaviour
+{
+    private float m_time;
+    public float SushiSpawnTime = 2.5f;
+    public float speed = 1;
+    public GameObject sushi;
+    bool Isleft = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Vector3 movementposion = Vector3.left * -speed;
+        if (transform.position.x <= -2)
+        {
+            Isleft = false;
+
+        }
+        if (transform.position.x >= 2)
+        {
+            Isleft = true;
+        }
+        if (Isleft == false)
+        {
+             movementposion.x = speed;
+        }
+        
+        if (Isleft == true)
+        {
+             movementposion.x = -speed;
+        }
+        transform.Translate(movementposion * Time.deltaTime, Space.World);
+        m_time += Time.deltaTime;
+        if (m_time >= SushiSpawnTime)
+        {
+            Instantiate(sushi, transform.position, Quaternion.Euler(90,0,0), null);
+            m_time = 0;
+        }
+    }
+}
